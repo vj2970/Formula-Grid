6c2c7783a6452c7695dc5ec98e083a4e93458262
vj2970 <vaibhavjha83@gmail.com>
2025-07-22 00:29:45 +0530

    Demo Login functionality

--- Back-End/src/main/java/com/vaibhav/formulaGrid/controller/UsersController.java
+++ Back-End/src/main/java/com/vaibhav/formulaGrid/controller/UsersController.java
@@ -1,10 +1,36 @@
 package com.vaibhav.formulaGrid.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/private-api/users")
+import com.vaibhav.formulaGrid.entity.Users;
+import com.vaibhav.formulaGrid.service.UsersService;
+
+@Controller
+// @RequestMapping("/private-api/users")
 public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @GetMapping("/getAllUsers")
+    public List<Users> getAllUsers(){
+        return usersService.getAllUsers();
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome.html";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login.html";
+    }
 
 }
--- Back-End/src/main/java/com/vaibhav/formulaGrid/security/Security.java
+++ Back-End/src/main/java/com/vaibhav/formulaGrid/security/Security.java
@@ -2,20 +2,38 @@ package com.vaibhav.formulaGrid.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Security {
 
+    // @Bean
+    // public static PasswordEncoder passwordEncoder(){
+    //     return new BCryptPasswordEncoder(12);
+    // }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest().permitAll())
+                    .requestMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/welcome", true)
+                )
+                .logout(logout -> logout
+                    .logoutSuccessUrl("/login")
+                    .permitAll()
+                )
                 .build();
     }
 
