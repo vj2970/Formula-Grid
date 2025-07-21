d73c64146758f68f7a4873dd7315f7fb4a54fa9d
vj2970 <vaibhavjha83@gmail.com>
2025-07-21 23:07:12 +0530

    .

---
+++ Back-End/src/main/java/com/vaibhav/formulaGrid/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.vaibhav.formulaGrid.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private-api/users")
+public class UsersController {
+
+}
