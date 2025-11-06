ae4396fea2ffd10b8bc5362b45e7dcfb1a1d9191
Vaibhav Jha <99133919+vj2970@users.noreply.github.com>
2025-11-06 22:58:43 +0530

    Added Initial Files
    Added the initial files.

---
+++ backend/src/main/java/com/formulagrid/FormulaGrid/FormulaGridApplication.java
@@ -0,0 +1,13 @@
+package com.formulagrid.FormulaGrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaGridApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/formulagrid/FormulaGrid/FormulaGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formulagrid.FormulaGrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
